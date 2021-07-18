40467186504ddb89ec819d0608e2a1caca96c8c9
jcorozco81 <jcorozco@gmail.com>
2021-07-18 19:09:53 -0400

    added write to file feature

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-// Dependencies
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+let jsonFileRaw = fs.readFileSync('db/db.json');
+let jsonData = JSON.parse(jsonFileRaw);
+console.log(jsonData);
+
+
+
+
 
 // For Test only
 const tableData = require('./db/tableData'); //For test only
+const { response } = require('express');
 
 
 // Sets up the Express App
 
 const app = express();
-const PORT = 3030;
+const PORT = process.env.PORT || 3030;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -24,31 +33,40 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 
-app.get('/api/notes', (req, res) => res.json(tableData));
+app.get('/api/notes', (req, res) => {
+res.json(jsonData);
+
+
+
+});
+
+
 
 
 app.post('/api/notes', (req, res) => {
 
     const newNote = req.body;
- 
-    console.log(newNote);
-  
-    tableData.push(newNote);
-    console.log(tableData);
+    console.log(newNote); 
+    jsonData.push(newNote);
+    writeToFile(jsonData);
+    jsonFileRaw = fs.readFileSync('db/db.json');
+    jsonData = JSON.parse(jsonFileRaw);
+    console.log(jsonData);
     res.json(newNote);
 
 });
 
 app.delete('/api/notes/:id', (req, res) => 
 {
-
+    jsonFileRaw = fs.readFileSync('db/db.json');
+    jsonData = JSON.parse(jsonFileRaw);
     const toDelete = req.params.id;
+    for (let i = 0; i < jsonData.length; i++) {
+        if (toDelete === jsonData[i].id) {
+            jsonData.splice(i, 1);
+            writeToFile(jsonData);
+          return res.json(jsonData);
 
-    for (let i = 0; i < tableData.length; i++) {
-        if (toDelete === tableData[i].id) {
-            tableData.splice(i, 1);
-
-          return res.json(tableData);
         }
       }
  
@@ -59,8 +77,22 @@ app.delete('/api/notes/:id', (req, res) =>
 });
 
 
+function writeToFile(dataToWrite){
+  const stringifiedData = JSON.stringify(dataToWrite);
+  fs.writeFile('db/db.json', stringifiedData, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+  })
+ }
 
-
+//  function readFromFile() {
+//   let jsonFileRaw = fs.readFileSync('db/db.json');
+//   let jsonData = JSON.parse(jsonFileRaw);
+//   console.log(jsonData);
+//   return (data);
+//   }
 
 
 // Starts the server to begin listening
