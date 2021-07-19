d76ce2fc4a6b001de9e9c8171f92e2652842a369
jcorozco81 <jcorozco@gmail.com>
2021-07-18 22:48:25 -0400

    Added new unique ID package

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # note-taker
 A Note Taker
-
-
-https://www.npmjs.com/package/node-json-db
-https://openbase.com/js/node-json-db/documentation
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+const uniqid = require('uniqid'); //Unique ID Package
+
 let jsonFileRaw = fs.readFileSync('db/db.json');
 let jsonData = JSON.parse(jsonFileRaw);
 console.log(jsonData);
@@ -12,11 +14,10 @@ console.log(jsonData);
 
 // For Test only
 const tableData = require('./db/tableData'); //For test only
-const { response } = require('express');
 
 
-// Sets up the Express App
 
+// Sets Express
 const app = express();
 const PORT = process.env.PORT || 3030;
 
@@ -26,60 +27,51 @@ app.use(express.json());
 
 
 // Routes
-
 app.use(express.static('public')); //from https://stackoverflow.com/questions/24582338/how-can-i-include-css-files-using-node-express-and-ejs
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-
-app.get('/api/notes', (req, res) => {
-res.json(jsonData);
-
-
-
-});
-
-
-
+// api Routes
+app.get('/api/notes', (req, res) => res.json(jsonData));
 
 app.post('/api/notes', (req, res) => {
-
-    const newNote = req.body;
-    console.log(newNote); 
-    jsonData.push(newNote);
-    writeToFile(jsonData);
-    jsonFileRaw = fs.readFileSync('db/db.json');
-    jsonData = JSON.parse(jsonFileRaw);
-    console.log(jsonData);
-    res.json(newNote);
+    let newNote = req.body;
+    //Obsolete function
+    // newNote.id = (parseInt(checkID())+1).toString(); 
+      newNote.id = uniqid.time('note-');
+      jsonData.push(newNote);
+      console.log("Note added");
+      writeToFile(jsonData);
+      return res.end();
 
 });
+
+
+
 
 app.delete('/api/notes/:id', (req, res) => 
 {
-    jsonFileRaw = fs.readFileSync('db/db.json');
-    jsonData = JSON.parse(jsonFileRaw);
+    // jsonFileRaw = fs.readFileSync('db/db.json');
+    // jsonData = JSON.parse(jsonFileRaw);
     const toDelete = req.params.id;
     for (let i = 0; i < jsonData.length; i++) {
-        if (toDelete === jsonData[i].id) {
+        if (toDelete == jsonData[i].id) {
             jsonData.splice(i, 1);
-            writeToFile(jsonData);
-          return res.json(jsonData);
-
+            console.log("Note deleted");
+            writeToFile(jsonData);            
+            return res.end();
         }
       }
- 
-  
-    console.log(tableData);
-    res.json(newNote);
+    });
 
-});
-
-
+// Function to write to JSON file
 function writeToFile(dataToWrite){
   const stringifiedData = JSON.stringify(dataToWrite);
   fs.writeFile('db/db.json', stringifiedData, err => {
+    jsonFileRaw = fs.readFileSync('db/db.json');
+    jsonData = JSON.parse(jsonFileRaw);
+    console.log("File updated.")
     if (err) {
       console.error(err)
       return
@@ -95,6 +87,17 @@ function writeToFile(dataToWrite){
 //   }
 
 
-// Starts the server to begin listening
+//Obsolete function
+// function checkID(){
+//   let idTest = "0";
+//   jsonData.forEach(element => {
+//     console.log(`Element ID: ${element.id}`);
+//     if (element.id > idTest){
+//       idTest=element.id;
+//     }});
+//   return idTest;
+// }
 
+
+// Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
